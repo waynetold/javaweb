620810f5b43255f000935bb035a052c9d95a2a0d
cbdyzj <cbdyzj@jianzhao.org>
2018-04-15 22:32:29 +0800

    Update front end

--- src/main/java/org/zhangwenli/web/controller/UserController.java
+++ src/main/java/org/zhangwenli/web/controller/UserController.java
@@ -3,6 +3,7 @@ package org.zhangwenli.web.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.zhangwenli.web.entity.User;
@@ -10,6 +11,7 @@ import org.zhangwenli.web.service.UserService;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpSession;
+import java.util.Objects;
 
 @Api(tags = "用户管理")
 @RestController
@@ -35,8 +37,11 @@ public class UserController {
 
     @ApiOperation("当前用户")
     @RequestMapping(path = "/me", method = RequestMethod.GET)
-    public ResponseEntity login(@ApiIgnore @SessionAttribute("user") User user) {
-        return ResponseEntity.ok(user);
+    public ResponseEntity login(@ApiIgnore @SessionAttribute(name = "user", required = false) User user) {
+        if (Objects.nonNull(user)) {
+            return ResponseEntity.ok(user);
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @ApiOperation("登出")
